746e23ee42debe307a5ac748e577221c7ed0cc93
Daniel Fairhead <daniel@dev.ngo>
2025-05-09 09:17:49 +0100

    initial working, post notifications to MiniNotify

--- lua/diagflow/lazy.lua
+++ lua/diagflow/lazy.lua
@@ -1,60 +1,11 @@
+
+
 local M = {}
-
-local strlen = vim.fn.strdisplaywidth
-
-local function create_boxed_text(text_lines, add_boxed_text)
-    if #text_lines == 0 or not add_boxed_text then
-        return text_lines
-    end
-    local border_chars = M.config.border_chars
-
-    local max_length = 0
-    for _, line in ipairs(text_lines) do
-        max_length = math.max(max_length, strlen(line))
-    end
-
-    local top_border = border_chars.top_left .. string.rep(border_chars.horizontal, max_length) .. border_chars.top_right
-    local bottom_border = border_chars.bottom_left .. string.rep(border_chars.horizontal, max_length) .. border_chars.bottom_right
-    local boxed_lines = {top_border}
-
-    for _, line in ipairs(text_lines) do
-        local padded_line = line .. string.rep(" ", max_length - strlen(line))
-        table.insert(boxed_lines, border_chars.vertical .. padded_line .. border_chars.vertical)
-    end
-
-    table.insert(boxed_lines, bottom_border)
-    return boxed_lines
-end
 
 local function len(T)
     local count = 0
     for _ in pairs(T) do count = count + 1 end
     return count
-end
-
-local function wrap_text(text, max_width)
-    local lines = {}
-
-    for line in text:gmatch("([^\n]*)\n?") do
-        local wrapped_line = ""
-        for word in line:gmatch("%S+") do
-            if strlen(wrapped_line) + strlen(word) + 1 > max_width then
-                local trimmed_line = vim.trim(wrapped_line)
-                if trimmed_line ~= "" then
-                    table.insert(lines, trimmed_line)
-                end
-                wrapped_line = word
-            else
-                wrapped_line = wrapped_line ~= "" and wrapped_line .. " " .. word or word
-            end
-        end
-        local trimmed_line = vim.trim(wrapped_line)
-        if trimmed_line ~= "" then
-            table.insert(lines, trimmed_line)
-        end
-    end
-
-    return lines
 end
 
 local group = nil
@@ -65,6 +16,7 @@ local error = function(message)
 end
 
 M.cached = {}
+M.notifications = {}
 
 local function update_cached_diagnostic()
 	local bufnr = vim.api.nvim_get_current_buf()
@@ -147,6 +99,11 @@ function M.init(config)
         -- Clear existing extmarks
         vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
 
+        for k, notif in ipairs(M.notifications) do
+            MiniNotify.remove(notif)
+            M.notifications[k] = nil
+        end
+
         local win_info = vim.fn.getwininfo(vim.fn.win_getid())[1]
 
         local diags = M.cached[vim.api.nvim_get_current_buf()] or {}
@@ -164,76 +121,31 @@ function M.init(config)
                 end
         end
 
-        local severity = {
-            [vim.diagnostic.severity.ERROR] = config.severity_colors.error,
-            [vim.diagnostic.severity.WARN] = config.severity_colors.warn,
-            [vim.diagnostic.severity.INFO] = config.severity_colors.info,
-            [vim.diagnostic.severity.HINT] = config.severity_colors.hint,
+        local notification_level_lookup = {
+            [vim.diagnostic.severity.ERROR] = 'ERROR',
+            [vim.diagnostic.severity.WARN] = 'WARN',
+            [vim.diagnostic.severity.INFO] = 'INFO',
+            [vim.diagnostic.severity.HINT] = 'DEBUG',
         }
 
-        local line_offset = 0
-        local win_width = win_info.width - win_info.textoff - config.padding_right
+
+        local hl_group_lookup = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+            [vim.diagnostic.severity.WARN] = 'DiagnosticWarn',
+            [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+            [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+        }
+
         -- Render current_pos_diags
         for _, diag in ipairs(current_pos_diags) do
             local diag_message = config.format(diag)
 
-            local hl_group = severity[diag.severity]
+            local level = notification_level_lookup[diag.severity]
+            local hl_group = hl_group_lookup[diag.severity]
             local sign = config.show_sign and signs[vim.diagnostic.severity[diag.severity]] .. " " or ""
-            local message_lines = wrap_text(sign .. diag_message, config.max_width)
-            message_lines = create_boxed_text(message_lines, config.show_borders)
 
-            local max_width = 0
-            if config.text_align == 'left' then
-                for _, message in ipairs(message_lines) do
-                    max_width = math.max(max_width, strlen(message))
-                end
-            end
+            table.insert(M.notifications, MiniNotify.add(diag_message, level, hl_group, {source='lsp_diagnostic'}))
 
-            local is_right = config.text_align == 'right'
-            local is_top = config.placement == 'top'
-
-            local lines_added = 0
-            for idx, message in ipairs(message_lines) do
-                if lines_added >= config.max_height then
-                    break
-                end
-                lines_added = lines_added + 1
-                if config.placement == 'inline' then
-                    -- output message lines starting from the last one
-                    local last_line = math.min(config.max_height, #message_lines)
-                    local text = message_lines[last_line - idx + 1]
-                    local spacing = string.rep(" ", config.inline_padding_left)
-                    vim.api.nvim_buf_set_extmark(bufnr, ns, diag.lnum, diag.col, {
-                        virt_text_pos = 'eol',
-                        virt_text = { { spacing .. text, hl_group } },
-                        virt_text_hide = true,
-                        strict = false
-                    })
-                elseif is_top and is_right and config.padding_right == 0 then
-                    -- fixes the issue of neotree and nvim-tree weird not on screen when opened
-                    vim.api.nvim_buf_set_extmark(bufnr, ns, win_info.topline + line_offset + config.padding_top, 0, {
-                        virt_text_pos = 'right_align',
-                        virt_text = { { message, hl_group } },
-                        virt_text_hide = true,
-                        strict = false,
-                    })
-                else
-                    local align = config.text_align == 'left' and max_width or strlen(message)
-                    vim.api.nvim_buf_set_extmark(bufnr, ns, win_info.topline + line_offset + config.padding_top, 0, {
-                        virt_text_win_col = win_width - align,
-                        virt_text = { { message, hl_group } },
-                        virt_text_hide = true,
-                        strict = false
-                    })
-                end
-
-                line_offset = line_offset + 1
-            end
-
-            -- Add a gap only after each diagnostic, not after each line
-            if config.gap_size > 0 then
-                line_offset = line_offset + config.gap_size - 1
-            end
         end
     end
     local function toggle()
